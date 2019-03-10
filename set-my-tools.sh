2cc4e0e28bef7a0d2904e2e546d9bc85c0ffb228
Saberon <maryjean.saberon@p51189.intra.rakuten.co.jp>
2019-03-11 02:46:30 +0900

    additional install setup

---
+++ set-my-tools.sh
@@ -0,0 +1,12 @@
+# brew install php-cs-fixer 
+# brew install composer
+
+[ -e `which code` ] && code --install-extension DavidAnson.vscode-markdownlint
+
+[ ! -e "/Applications/Spectacle.app" ] && brew cask install spectacle
+[ ! -e "/Applications/Google Chrome.app" ] && brew cask install google-chrome
+
+brew cask install google-chrome-canary
+brew cask install firefox
+brew cask install firefox-developer-edition
+
