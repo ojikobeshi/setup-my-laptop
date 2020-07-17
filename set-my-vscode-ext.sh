18a7e95c52a39d86a222ad61be93113a711e52dc
MJ Saberon <maryjean.saberon@rakuten.com>
2020-07-17 20:31:01 +0900

    read extension name from file to install

--- set-my-vscode-ext.sh
+++ set-my-vscode-ext.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-[ -e `which code` ] && code --install-extension DavidAnson.vscode-markdownlint
-[ -e `which code` ] && code --install-extension letmaik.git-tree-compare
-[ -e `which code` ] && code --install-extension eamodio.gitlens
-[ -e `which code` ] && code --install-extension hookyqr.beautify
-[ -e `which code` ] && code --install-extension marcostazi.vs-code-vagrantfile
+# [ -e `which code` ] && code --install-extension DavidAnson.vscode-markdownlint
+# [ -e `which code` ] && code --install-extension letmaik.git-tree-compare
+# [ -e `which code` ] && code --install-extension eamodio.gitlens
+# [ -e `which code` ] && code --install-extension hookyqr.beautify
+# [ -e `which code` ] && code --install-extension marcostazi.vs-code-vagrantfile
 
-[ -e `which code` ] && code --install-extension octref.vetur
-[ -e `which code` ] && code --install-extension dbaeumer.vscode-eslint
-[ -e `which code` ] && code --install-extension cymonk.sql-formatter
-[ -e `which code` ] && code --install-extension arjun.swagger-viewer
+# [ -e `which code` ] && code --install-extension octref.vetur
+# [ -e `which code` ] && code --install-extension dbaeumer.vscode-eslint
+# [ -e `which code` ] && code --install-extension cymonk.sql-formatter
+# [ -e `which code` ] && code --install-extension arjun.swagger-viewer
 
-[ -e `which code` ] && code --install-extension wix.vscode-import-cost
+# [ -e `which code` ] && code --install-extension wix.vscode-import-cost
 
 
 # this may cause conflict with eslint (and vetur?)
@@ -21,7 +21,6 @@
 # https://laracasts.com/series/visual-studio-code-for-php-developers
 
 
-
 # wix.vscode-import-cost
 # patbenatar.advanced-new-file
 
@@ -29,11 +28,19 @@
 # felixfbecker.php-intellisense
 # felixfbecker.php-debug
 
-
-[ -e `which code` ] && code --install-extension bmewburn.vscode-intelephense-client
-[ -e `which code` ] && code --install-extension christian-kohler.npm-intellisense
-
-
+# [ -e `which code` ] && code --install-extension bmewburn.vscode-intelephense-client
+# [ -e `which code` ] && code --install-extension christian-kohler.npm-intellisense
 
 # ms-vsliveshare.vsliveshare
 # marabesi.php-import-checker
+
+while read extname
+do
+    if [[ $extname =~ ^# ]]
+    then
+        echo "Skip ${extname}.";
+    else
+        echo "Installing ${extname}..."
+        [ -e `which code` ] && code --install-extension  $extname && echo "Done."
+    fi
+done < ./vscode-extension.list
